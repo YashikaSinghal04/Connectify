dba8b625f807b1a8effce385be4b0528b2a51189
YashikaSinghal04 <yashikasinghal6@gmail.com>
2025-07-24 19:04:00 +0530

    Update server.js

--- BACKEND/SocketIO/server.js
+++ BACKEND/SocketIO/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
        origin: ["http://localhost:3001", "http://localhost:3002", "http://localhost:3000", "http://localhost:5173",
-                    "https://frolicking-cobbler-805b0d.netlify.app"  ],
+                    "https://jolly-tulumba-744a66.netlify.app"  ],
         methods: ["GET", "POST"],
         credentials: true
     }
